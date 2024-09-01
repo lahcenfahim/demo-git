1e0eb7b6c6dbf15044091f65a7fb94e954953302
lahcenfahim <fahim.etu@gmail.com>
2024-09-01 15:20:27 +0200

    Test github app

--- get_jwt_token.py
+++ get_jwt_token.py
@@ -2,17 +2,14 @@ import jwt
 import time
 import argparse
 
-def generate_jwt(app_id, private_key_path):
+def generate_jwt(app_id, private_key):
     """
     Generate a JWT (JSON Web Token) for authenticating a GitHub App.
-    
+
     :param app_id: GitHub App ID
-    :param private_key_path: Path to the GitHub App private key (PEM file)
+    :param private_key: GitHub App private key as a string
     :return: JWT token as a string
     """
-    with open(private_key_path, "r") as key_file:
-        private_key = key_file.read()
-
     # Current time
     now = int(time.time())
 
@@ -25,19 +22,19 @@ def generate_jwt(app_id, private_key_path):
 
     # Generate the JWT
     token = jwt.encode(payload, private_key, algorithm="RS256")
-    
+
     return token
 
 if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser(description="Generate a JWT for GitHub App authentication.")
     parser.add_argument('--app_id', type=str, required=True, help="GitHub App ID")
-    parser.add_argument('--private_key_path', type=str, required=True, help="Path to the GitHub App private key (PEM file)")
-    
+    parser.add_argument('--private_key', type=str, required=True, help="GitHub App private key (PEM format)")
+
     args = parser.parse_args()
 
     # Generate JWT
-    jwt_token = generate_jwt(args.app_id, args.private_key_path)
-    
+    jwt_token = generate_jwt(args.app_id, args.private_key)
+
     # Print the token (this can be captured in the workflow)
     print(jwt_token)
