feddd8994c7324f88cc0d89bf5381b454b4a2f5a
Lahcen EL-FAHIM <elfahim.lhcen@gmail.com>
2025-03-23 09:40:49 +0100

    Update hello-action with customizable greeting

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const core = require('@actions/core');
 function run() {
     try {
         const name = core.getInput('name');
-        const greeting = `Hello, ${name}!`;
-        console.log(greeting);
-        core.setOutput("greeting", greeting);
+        const greeting = core.getInput('greeting');
+        const message = `${greeting}, ${name}!`;
+        console.log(message);
+        core.setOutput("message", message);
     } catch (error) {
         core.setFailed(error.message);
     }
