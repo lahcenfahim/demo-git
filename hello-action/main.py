e6e6f7d947886a53e13ed341a64933e51d92860c
Lahcen EL-FAHIM <elfahim.lhcen@gmail.com>
2025-03-23 11:14:06 +0100

    debug

--- hello-action/main.py
+++ hello-action/main.py
@@ -4,9 +4,10 @@ def main():
     name = os.getenv('NAME', 'World')
     greeting = os.getenv('GREETING', 'Hello')
     message = f"{greeting}, {name}!"
-    
-    # Print the message (this will be captured as output)
-    print(f"::set-output name=message::{message}")
+
+    # Set output using environment files
+    with open(os.environ['GITHUB_ENV'], 'a') as env_file:
+        env_file.write(f"message={message}\n")
 
 if __name__ == "__main__":
     main()
